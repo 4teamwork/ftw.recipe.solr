7b618107b09fbfdfc6091b9d0294ee84886ae3dc
Thomas Buchberger <t.buchberger@4teamwork.ch>
2018-05-25 13:22:43 +0200

    Update default download url
    Use current version 7.3.1

--- ftw/recipe/solr/defaults.py
+++ ftw/recipe/solr/defaults.py
@@ -1,6 +1,6 @@
 DEFAULT_OPTIONS = {
-    'url': 'http://www-eu.apache.org/dist/lucene/solr/7.2.1/solr-7.2.1.tgz',
-    'md5sum': 'c9a2a610a20064228f42dad8b1128b46',
+    'url': 'http://archive.apache.org/dist/lucene/solr/7.3.1/solr-7.3.1.tgz',
+    'md5sum': '042a6c0d579375be1a8886428f13755f',
     'host': 'localhost',
     'port': '8983',
     'jvm-opts': '-Xms512m -Xmx512m -Xss256k',
